556a8a5dad827bc62953ea9a5471c6a981b78c2c
Admin <admin@vsplblrlt0192.local>
2018-03-06 23:37:54 +0530

    test commit

--- DbDemoExampleSwift/AppDelegate.swift
+++ DbDemoExampleSwift/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         Util.copyFile("Chats.sqlite")
         
-        
-        
+    ///hey hello
         
         return true
     }
